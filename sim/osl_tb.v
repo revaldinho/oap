046d780ef53ad1692f4561b4aa4e2c18bdb30c69
revaldinho <richevans@mac.com>
2020-07-29 18:37:07 +0100

    Add chip_sel input to osl_rxtx and clean up trailing whitespace

--- sim/osl_tb.v
+++ sim/osl_tb.v
@@ -7,12 +7,12 @@ module osl_tb;
   reg tx_r;
   reg hostA_rd_r;
   reg hostB_rd_r;
-  reg hostA_wr_r;  
-  reg hostB_wr_r; 
+  reg hostA_wr_r;
+  reg hostB_wr_r;
   wire [`WORDSZ-1:0] hostA_dout_w;
-  reg [`WORDSZ-1:0] hostA_din_r;  
+  reg [`WORDSZ-1:0] hostA_din_r;
   wire [`WORDSZ-1:0] hostB_dout_w;
-  reg [`WORDSZ-1:0] hostB_din_r;  
+  reg [`WORDSZ-1:0] hostB_din_r;
 
   wire data0_w;
   wire data1_w;
@@ -24,11 +24,11 @@ module osl_tb;
   task transmitAB;
     input [`WORDSZ-1:0] data;
     begin
-      hostA_din_r = data;            
+      hostA_din_r = data;
       // Wait for transmitter to be ready
-      while ( !hostA_dir_w )       
-        @(posedge clk_r);     
-      // Write data into the TX unit      
+      while ( !hostA_dir_w )
+        @(posedge clk_r);
+      // Write data into the TX unit
       @(posedge clk_r);
       hostA_wr_r = 1'b1;
       @(posedge clk_r);
@@ -39,23 +39,23 @@ module osl_tb;
       // Read data from the RX unit and compare
       @(posedge clk_r);
       hostB_rd_r = 1'b1;
-      if ( hostB_dout_w == data) 
+      if ( hostB_dout_w == data)
         $display ("PASS: RX matches TX data, expected 0x%4H, actual 0x%04H", data, hostB_dout_w) ;
-      else 
+      else
         $display("FAIL: RX, TX mismatch, expected 0x%4H, actual 0x%4H", data, hostB_dout_w);
       @(posedge clk_r);
-      hostB_rd_r = 1'b0;          
+      hostB_rd_r = 1'b0;
     end
-  endtask    
+  endtask
 
   task transmitBA;
     input [`WORDSZ-1:0] data;
     begin
-      hostB_din_r = data;            
+      hostB_din_r = data;
       // Wait for transmitter to be ready
-      while ( !hostB_dir_w )       
-        @(posedge clk_r);     
-      // Write data into the TX unit      
+      while ( !hostB_dir_w )
+        @(posedge clk_r);
+      // Write data into the TX unit
       @(posedge clk_r);
       hostB_wr_r = 1'b1;
       @(posedge clk_r);
@@ -66,15 +66,15 @@ module osl_tb;
       // Read data from the RX unit and compare
       @(posedge clk_r);
       hostA_rd_r = 1'b1;
-      if ( hostA_dout_w == data) 
+      if ( hostA_dout_w == data)
         $display ("PASS: RX matches TX data, expected 0x%4H, actual 0x%04H", data, hostA_dout_w) ;
-      else 
+      else
         $display("FAIL: RX, TX mismatch, expected 0x%4H, actual 0x%4H", data, hostA_dout_w);
       @(posedge clk_r);
-      hostA_rd_r = 1'b0;          
+      hostA_rd_r = 1'b0;
     end
-  endtask    
-  
+  endtask
+
   initial
     begin
 
@@ -83,58 +83,59 @@ module osl_tb;
 `else
        $dumpfile("osl_tb.vcd");
 `endif
-      $dumpvars();       
+      $dumpvars();
       clk_r = 0;
       rstb_r = 0;
       tx_r = 0;
       hostA_rd_r = 0;
-      hostA_wr_r = 1'b0;            
+      hostA_wr_r = 1'b0;
       hostB_rd_r = 0;
-      hostB_wr_r = 1'b0;            
+      hostB_wr_r = 1'b0;
       # 1000;
       @ (posedge clk_r );
-      rstb_r = 1;      
+      rstb_r = 1;
       # 1000;
-      $display("Sending data from A to B");      
+      $display("Sending data from A to B");
       transmitAB( `WORDSZ'h01234567);
       transmitAB( `WORDSZ'h89ABCDEF);
       transmitAB( `WORDSZ'h00112233);
       transmitAB( `WORDSZ'h44556677);
 
-      $display("Sending data from B to A");      
+      $display("Sending data from B to A");
       transmitBA( `WORDSZ'h01234567);
       transmitBA( `WORDSZ'h89ABCDEF);
       transmitBA( `WORDSZ'h00112233);
-      transmitBA( `WORDSZ'h44556677);      
+      transmitBA( `WORDSZ'h44556677);
       #10000 $finish();
     end
 
   always
     #`CLKPHASE  clk_r <= !clk_r;
-  
-  osl_rxtx UA_0 (
-                .clk(clk_r),
-                .resetb(rstb_r),              
-                .rx(data0_w),
-                .tx(data1_w),
-                .host_rd(hostA_rd_r),
-                .host_dout(hostA_dout_w),
-                .host_dor(hostA_dor_w),
-                .host_wr(hostA_wr_r),
-                .host_din(hostA_din_r),
-                .host_dir(hostA_dir_w)           
-                );
-  osl_rxtx UB_1 (
-                .clk(clk_r),
-                .resetb(rstb_r),              
-                .rx(data1_w),
-                .tx(data0_w),
-                .host_rd(hostB_rd_r),
-                .host_dout(hostB_dout_w),
-                .host_dor(hostB_dor_w),
-                .host_wr(hostB_wr_r),
-                .host_din(hostB_din_r),
-                .host_dir(hostB_dir_w)           
-                );  
-endmodule
 
+  osl_rxtx UA_0 (
+                 .clk(clk_r),
+                 .resetb(rstb_r),
+                 .rx(data0_w),
+                 .tx(data1_w),
+                 .chip_sel(1'b1),
+                 .host_rd(hostA_rd_r),
+                 .host_dout(hostA_dout_w),
+                 .host_dor(hostA_dor_w),
+                 .host_wr(hostA_wr_r),
+                 .host_din(hostA_din_r),
+                 .host_dir(hostA_dir_w)
+                 );
+  osl_rxtx UB_1 (
+                 .clk(clk_r),
+                 .resetb(rstb_r),
+                 .rx(data1_w),
+                 .tx(data0_w),
+                 .chip_sel(1'b1),
+                 .host_rd(hostB_rd_r),
+                 .host_dout(hostB_dout_w),
+                 .host_dor(hostB_dor_w),
+                 .host_wr(hostB_wr_r),
+                 .host_din(hostB_din_r),
+                .host_dir(hostB_dir_w)
+                 );
+endmodule
--- src/osl_rxtx.v
+++ src/osl_rxtx.v
@@ -1,45 +1,49 @@
 `include "osl.h"
 
 module osl_rxtx(
-              input clk,
-              input resetb,              
+              input                  clk,
+              input                  resetb,
+              input                  chip_sel, 
+                
 
-              input rx,
-              output tx,
+              input                  rx,
+              output                 tx,
 
-              input  host_rd,
+              input                  host_rd,
               output [ `WORDSZ-1: 0] host_dout,
-              output host_dor,
+              output                 host_dor,
 
-              input  host_wr,
-              input [ `WORDSZ-1: 0] host_din,
-              output host_dir           
+              input                  host_wr,
+              input [ `WORDSZ-1: 0]  host_din,
+              output                 host_dir
            );
 
-  parameter RXIDLE=2'b00, RXDATA=2'b10, RXREQACK=2'b11, RXWAITACK=2'b01;
-  parameter TXIDLE=3'b000, TXDATA=3'b010, TXWAITACK=3'b011, TXRDY=3'b001, 
+  parameter RXIDLE=3'b000, RXDATA=3'b010, RXREQACK=3'b011, RXWAITACK=3'b001, RXRDY=3'b100;
+  parameter TXIDLE=3'b000, TXDATA=3'b010, TXWAITACK=3'b011, TXRDY=3'b001,
     TXHEADER0=3'b100, TXHEADER1=3'b101, TXSENDACK0=3'b110, TXSENDACK1=3'b111;
 
-`ifdef TX_ON_NEGEDGE  
+`ifdef TX_ON_NEGEDGE
   reg [1:0]		din_q;
 `else
   reg	                din_q;
-`endif  
+`endif
   reg [`WORDSZ-1:0]     ser2par_q;
   reg [`COUNTERSZ-1:0] 	rx_count_q;
-  reg [1:0]             rx_state_q;
+  reg [2:0]             rx_state_q;
+  reg [2:0]             tx_state_q;
   reg	                rx_ready_q;
   reg [`WORDSZ-1:0]     par2ser_q;
   reg [`COUNTERSZ-1:0] 	tx_count_q;
-  reg [2:0]             tx_state_q;
-  reg	                tx_ready_q; 
+  reg	                tx_ready_q;
   reg                   tx_r;
   reg			tx_ack_rcvd_q;
   reg 			rx_ack_req_q;
-  reg 			rx_ack_sent_q;  
+  reg 			rx_ack_sent_q;
   wire [1:0]            din_w;
-
+  wire                  host_rd_w = host_rd & chip_sel;
+  wire                  host_wr_w = host_wr & chip_sel;
   
+
   assign host_dor = !rx_ready_q;
   assign host_dout = ser2par_q;
   assign host_dir = !tx_ready_q;
@@ -47,11 +51,11 @@ module osl_rxtx(
 
 `ifdef TX_ON_NEGEDGE
   assign din_w = din_q;
-`else  
+`else
   assign din_w = {din_q, rx};
 `endif
-  
-  always @ ( * ) 
+
+  always @ ( * )
     case (tx_state_q)
       TXSENDACK0: tx_r = 1'b1;
       TXSENDACK1: tx_r = 1'b0;
@@ -65,7 +69,7 @@ module osl_rxtx(
     if ( !resetb)
       tx_state_q <= TXIDLE;
     else
-      case (tx_state_q) 
+      case (tx_state_q)
         TXIDLE: tx_state_q <= ( rx_ack_req_q) ? TXSENDACK0 : (tx_ready_q) ?  TXHEADER0 : TXIDLE;
         TXHEADER0: tx_state_q <= TXHEADER1;
         TXHEADER1: tx_state_q <= TXDATA;
@@ -76,20 +80,20 @@ module osl_rxtx(
         TXSENDACK1: tx_state_q <= TXIDLE;
         default: tx_state_q <= TXIDLE;
       endcase
-  
+
   always @ ( posedge clk or negedge resetb)
     if ( !resetb)
       tx_ready_q <= 1'b0;
-    else if (tx_state_q==TXRDY) 
+    else if (tx_state_q==TXRDY)
       tx_ready_q <= 1'b0;
-    else if (host_wr )
-      tx_ready_q <= 1'b1;      
-  
+    else if (host_wr_w )
+      tx_ready_q <= 1'b1;
+
   always @ ( posedge clk or negedge resetb)
     if (!resetb)
       tx_count_q <= 0;
-    else	
-      tx_count_q <= (tx_state_q == TXDATA) ? tx_count_q +1: (tx_state_q==TXIDLE) ? 0 : tx_count_q; 
+    else
+      tx_count_q <= (tx_state_q == TXDATA) ? tx_count_q +1: (tx_state_q==TXIDLE) ? 0 : tx_count_q;
 
   always @ ( posedge clk or negedge resetb)
     if ( !resetb)
@@ -99,7 +103,8 @@ module osl_rxtx(
         RXIDLE: rx_state_q <= (&din_w) ?  RXDATA : RXIDLE;
         RXDATA: rx_state_q <= ( rx_count_q != `WORDSZ-1 ) ? RXDATA : ( rx_ready_q ) ? RXREQACK : RXIDLE ;
         RXREQACK: rx_state_q <= RXWAITACK;
-        RXWAITACK: rx_state_q <= RXIDLE;
+        RXWAITACK: rx_state_q <= RXRDY;
+        RXRDY: rx_state_q <= RXIDLE;
         default: rx_state_q <= RXIDLE;
       endcase
 
@@ -107,7 +112,7 @@ module osl_rxtx(
     if ( !resetb) begin
       tx_ack_rcvd_q <= 0;
       rx_ack_req_q <= 0;
-    end  
+    end
     else begin
       if ( tx_state_q == TXHEADER0 )
         tx_ack_rcvd_q <= 0;
@@ -119,38 +124,38 @@ module osl_rxtx(
       else if ( tx_state_q == TXSENDACK1)
         rx_ack_req_q <= 0;
     end
-  
+
   always @ ( posedge clk or negedge resetb)
     if ( !resetb)
       rx_ready_q <= 1'b1;
-    else if (rx_state_q == RXREQACK) 
+    else if (rx_state_q == RXRDY)
       rx_ready_q <= 1'b0;
-    else if (host_rd )
-      rx_ready_q <= 1'b1;      
-  
+    else if (host_rd_w )
+      rx_ready_q <= 1'b1;
+
   always @ ( posedge clk or negedge resetb)
     if (!resetb)
       rx_count_q <= 0;
-    else	
+    else
       rx_count_q <= (rx_state_q == RXDATA) ? rx_count_q +1: (rx_state_q==RXIDLE) ? 0 : rx_count_q;
 
   always @ ( `TXEDGE clk or negedge resetb)
     if ( !resetb) begin
-      ser2par_q <= 0;      
+      ser2par_q <= 0;
       par2ser_q <= 0;
       din_q <= 0;
-    end 
+    end
     else begin
-      if ( host_wr && tx_state_q==TXIDLE)
+      if ( host_wr_w && tx_state_q==TXIDLE)
         par2ser_q <= host_din;
       else
         par2ser_q <= ( tx_ready_q & (tx_state_q==TXDATA) ) ? (par2ser_q << 1) : par2ser_q;
       ser2par_q <= ( rx_ready_q & (rx_state_q==RXDATA)) ? (ser2par_q<<1) | rx  : ser2par_q ;
-`ifdef TX_ON_NEGEDGE      
+`ifdef TX_ON_NEGEDGE
       din_q <= {din_q[0], rx} ;
-`else      
+`else
       din_q <= rx ;
-`endif      
+`endif
     end
-  
+
 endmodule
