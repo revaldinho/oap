516092d47cffb7db40322d41e51288cf0d0077a0
revaldinho <richevans@mac.com>
2020-08-06 18:38:22 +0100

    Remove unused DIN from ROM module

--- src/rom1024x32.v
+++ src/rom1024x32.v
@@ -1,7 +1,6 @@
 module rom1024x32 (
                    input         clk,
                    input [9:0]   address,
-                   input [31:0]  din,
                    output [31:0] dout
                    );
   
@@ -9,7 +8,6 @@ module rom1024x32 (
   
   reg [31:0]                     mem [ 1023:0 ] ;  (* RAM_STYLE="DISTRIBUTED" *)
   reg [9:0]                      address_q ;
-  reg [31:0]                     din_q;
   
   assign dout = mem[address_q];
 
